2a292a80890c7f52a7e41ef96e539ff93e391e3f
Trung-Cyberpunk <trungnguyen14061@gmail.com>
2023-04-18 14:47:23 +0700

    all

--- assets/js/sign_login.js
+++ assets/js/sign_login.js
@@ -1,24 +1,31 @@
-
+const backgr = document.querySelector(".backgr");
 // sign
 const btn_signUp = document.querySelector("#btn-header_signUp");
 const wapper_sign = document.querySelector(".signUp");
 const sign_close  = document.querySelector("#btn_x");
 const login_a  = document.querySelector("#login_a");
+const sign_a  = document.querySelector("#sign_a");
 
 btn_signUp.onclick = () => {
     wapper_sign.classList.add("active");
     wapper_login.classList.remove("active");
-
+    backgr.classList.add("opacity");
 }
 sign_close.onclick = () => {
     wapper_sign.classList.remove("active");
-    
-
+    backgr.classList.remove("opacity");
 }
 
 login_a.onclick = () =>{
     wapper_sign.classList.remove("active");
     wapper_login.classList.add("active");
+    backgr.classList.add("opacity");
+}
+
+sign_a.onclick = () =>{
+    wapper_sign.classList.add("active");
+    wapper_login.classList.remove("active");
+    backgr.classList.add("opacity");
 }
 
 
@@ -33,13 +40,54 @@ const login_close  = document.querySelector("#btn_y");
 btn_login.onclick = () => {
     wapper_login.classList.add("active");
     wapper_sign.classList.remove("active");
+    backgr.classList.add("opacity");
 
 }
 login_close.onclick = () => {
     wapper_login.classList.remove("active");
+    backgr.classList.remove("opacity");
 }
 sign_a.onclick = () =>{
     wapper_sign.classList.add("active");
     wapper_login.classList.remove("active");
 }
 
+// Forgot password
+
+const reset_close = document.querySelector("#btn_z");
+const reset_pw = document.querySelector(".reset_pw");
+const link_rs = document.querySelector("#passwork_rs");
+const login_rs = document.querySelector("#login_rs");
+
+link_rs.onclick = () =>{
+    reset_pw.classList.add("active");
+    wapper_login.classList.remove("active"); 
+    backgr.classList.add("opacity");    
+}
+
+login_rs.onclick = () => {
+    reset_pw.classList.remove("active");
+    wapper_login.classList.add("active");
+    backgr.classList.add("opacity");
+}
+
+reset_close.onclick = () => {
+    reset_pw.classList.remove("active");
+    backgr.classList.remove("opacity");
+}
+//reset
+
+const btn_reset = document.querySelector("#btn_reset_pw");
+const reset_pw_2 = document.querySelector(".reset_pw_2");
+const btn_z_2 = document.querySelector("#btn_z_2");
+
+btn_reset.onclick = () =>{
+    reset_pw_2.classList.add("active");
+    reset_pw.classList.remove("active");
+    backgr.classList.add("opacity");
+}
+
+btn_z_2.onclick = () => {
+    reset_pw_2.classList.remove("active");  
+    backgr.classList.remove("opacity");
+}
