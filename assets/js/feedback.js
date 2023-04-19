25d1f02675bf0ce743d492fb839dd4f9a2d82e01
Trung-Cyberpunk <trungnguyen14061@gmail.com>
2023-04-19 10:32:24 +0700

    all

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -13,3 +13,25 @@ remove_fb.onclick = () => {
     feedback_icon.classList.remove("run");    
     backgr.classList.remove("opacity");
 }
+
+
+//khảo sát hài lòng (sad, like, angry...)
+const imageContainers = document.querySelectorAll('.container_icon'); //chọn 1 phần tử chứa ảnh và text
+// nummm = imageContainers.length;
+// alert(nummm)
+
+imageContainers.forEach((container) => {
+    const image = container.querySelector('.feedback-img'); //khi bấm vào 1 ảnh
+
+    image.addEventListener('click', () => {
+        // Loại bỏ lớp CSS "selected" khỏi tất cả các phần tử "image-container"
+        imageContainers.forEach((container) => {
+            container.classList.remove('selected');
+        });
+        // Thêm lớp CSS "selected" vào phần tử "image-container" tương ứng với ảnh được chọn
+        container.classList.add('selected');
+    });
+});
+
+// dùng vòng lặp forEach để clear tất cả các .container_icon 
+//có selected và rồi thêm selected vào khối chọn
