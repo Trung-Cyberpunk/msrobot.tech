52f5747a8316fde40e87b4c273308ada15952ec2
Trung-Cyberpunk <trungnguyen14061@gmail.com>
2023-06-17 18:55:10 +0700

    all

--- assets/js/forum.js
+++ assets/js/forum.js
@@ -5,12 +5,13 @@ const ask_qs = document.querySelector("#ask_qs");
 const remove_ask = document.querySelector(".ask_btn");
 
 const ask_qsBox = document.querySelector(".ask_qsBox");
-
+// open ask qs box
 ask_qs.onclick = () => {
     ask_qsBox.classList.add("run");    
     backgr.classList.add("opacity");
 }
 
+// close ask qs box
 remove_ask.onclick = () => {
     ask_qsBox.classList.remove("run");    
     backgr.classList.remove("opacity");
--- assets/js/sign_box.js
+++
@@ -1,8 +0,0 @@
-
-
-
-ask_login.onclick = () =>{
-    wapper_sign.classList.add("sign_x");
-    backgr.classList.add("opacity");
-    wapper_login.classList.remove("active");
-}
